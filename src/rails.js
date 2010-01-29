7ae514ff734fddc2644fd0276f6f76523d35a504
Joshua Peek <josh@joshpeek.com>
2010-01-29 16:28:15 -0600

    Forward AJAX callbacks as custom events from the target element

--- src/rails.js
+++ src/rails.js
@@ -1,4 +1,34 @@
 document.observe("dom:loaded", function() {
+  var Callbacks = {
+    onUninitialized: function(element, request) {
+      element.fire("ajax:uninitialized", {request: request});
+    },
+
+    onLoading: function(element, request) {
+      element.fire("ajax:loading", {request: request});
+    },
+
+    onLoaded: function(element, request) {
+      element.fire("ajax:loaded", {request: request});
+    },
+
+    onInteractive: function(element, request) {
+      element.fire("ajax:interactive", {request: request});
+    },
+
+    onComplete: function(element, request) {
+      element.fire("ajax:complete", {request: request});
+    },
+
+    onSuccess: function(element, request) {
+      element.fire("ajax:success", {request: request});
+    },
+
+    onFailure: function(element, request) {
+      element.fire("ajax:failure", {request: request});
+    }
+  }
+
   function handleRemoteLink(element) {
     // TODO: data-url support is going away, just use href
     var url    = element.readAttribute('data-url') || element.readAttribute('href');
@@ -7,7 +37,15 @@ document.observe("dom:loaded", function() {
     new Ajax.Request(url, {
       method: method,
       asynchronous: true,
-      evalScripts: true
+      evalScripts: true,
+
+      onUninitialized: Callbacks.onUninitialized.curry(element),
+      onLoading: Callbacks.onLoading.curry(element),
+      onLoaded: Callbacks.onLoaded.curry(element),
+      onInteractive: Callbacks.onInteractive.curry(element),
+      onComplete: Callbacks.onComplete.curry(element),
+      onSuccess: Callbacks.onSuccess.curry(element),
+      onFailure: Callbacks.onFailure.curry(element)
     });
   }
 
