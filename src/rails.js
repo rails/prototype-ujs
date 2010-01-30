de24b339b41022b1696408972015236eb6511ebe
Joshua Peek <josh@joshpeek.com>
2010-01-29 21:36:44 -0600

    Observe and process remote forms

--- src/rails.js
+++ src/rails.js
@@ -25,12 +25,21 @@ document.observe("dom:loaded", function() {
     }
   }
 
-  function handleRemoteLink(element) {
-    var event;
+  function handleRemote(element) {
+    var method, url, params;
 
-    // TODO: data-url support is going away, just use href
-    var url    = element.readAttribute('data-url') || element.readAttribute('href');
-    var method = element.readAttribute('data-method') || 'GET';
+    if (element.tagName.toLowerCase() == 'form') {
+      method = element.readAttribute('method') || 'post';
+      url    = element.readAttribute('action');
+      params = element.serialize(true);
+    } else {
+      method = element.readAttribute('data-method') || 'get';
+      // TODO: data-url support is going away, just use href
+      url    = element.readAttribute('data-url') || element.readAttribute('href');
+      params = {};
+    }
+
+    var event;
 
     // TODO: Better name for this event
     event = element.fire("rails:confirm");
@@ -41,6 +50,7 @@ document.observe("dom:loaded", function() {
 
     new Ajax.Request(url, {
       method: method,
+      parameters: params,
       asynchronous: true,
       evalScripts: true,
 
@@ -58,7 +68,7 @@ document.observe("dom:loaded", function() {
   $(document.body).observe("click", function(event) {
     var element = event.findElement("a[data-remote=true]");
     if (element) {
-      handleRemoteLink(element);
+      handleRemote(element);
       event.stop();
     }
   });
@@ -71,10 +81,30 @@ document.observe("dom:loaded", function() {
 
   // TODO: I don't think submit bubbles in IE
   $(document.body).observe("submit", function(event) {
-    var inputs = event.element().select("input[data-disable-with]");
+    var inputs = event.element().select("input[type=submit][data-disable-with]");
     inputs.each(function(input) {
       input.disabled = true;
+      input.writeAttribute('data-original-value', input.value);
       input.value = input.readAttribute('data-disable-with');
     });
+
+    var element = event.findElement("form[data-remote=true]");
+    if (element) {
+      handleRemote(element);
+      event.stop();
+    }
+  });
+
+  $(document.body).observe("ajax:complete", function(event) {
+    var element = event.element();
+
+    if (element.tagName.toLowerCase() == 'form') {
+      var inputs = element.select("input[type=submit][disabled=true][data-disable-with]");
+      inputs.each(function(input) {
+        input.value = input.readAttribute('data-original-value');
+        input.writeAttribute('data-original-value', null);
+        input.disabled = false;
+      });
+    }
   });
 });
