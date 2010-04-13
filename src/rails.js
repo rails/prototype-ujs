1c22a119b5918edca7f9588763ab71cebb99da0f
Juriy Zaytsev <kangax@gmail.com>
2010-04-13 08:39:46 +0800

    `token_name` is not used, delete it; `token` is declared twice, remove one of them; Do not predeclare variables (better compression).

--- src/rails.js
+++ src/rails.js
@@ -32,12 +32,10 @@ document.observe("dom:loaded", function() {
   }
 
   function handleMethod(element) {
-    var method, url, token_name, token;
-
-    method     = element.readAttribute('data-method');
-    url        = element.readAttribute('href');
-    csrf_param = $$('meta[name=csrf-param]').first();
-    csrf_token = $$('meta[name=csrf-token]').first();
+    var method = element.readAttribute('data-method'), 
+        url = element.readAttribute('href'),
+        csrf_param = $$('meta[name=csrf-param]').first(),
+        csrf_token = $$('meta[name=csrf-token]').first();
 
     var form = new Element('form', { method: "POST", action: url, style: "display: none;" });
     element.parentNode.appendChild(form);
