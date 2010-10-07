c37c453361f18e24fc2e145b9dd27a50ac402643
Mislav Marohnić <mislav.marohnic@gmail.com>
2010-10-07 22:58:45 +0200

    fire the "ajax:create" event as part of the "data-remote" functionality
    this event is useful for modifying the request; e.g. adding HTTP headers

--- src/rails.js
+++ src/rails.js
@@ -95,9 +95,10 @@
       parameters: params,
       evalScripts: true,
 
-      onComplete:    function(request) { element.fire("ajax:complete", request); },
-      onSuccess:     function(request) { element.fire("ajax:success",  request); },
-      onFailure:     function(request) { element.fire("ajax:failure",  request); }
+      onCreate:   function(request)  { element.fire("ajax:create",   request); },
+      onComplete: function(response) { element.fire("ajax:complete", response); },
+      onSuccess:  function(response) { element.fire("ajax:success",  response); },
+      onFailure:  function(response) { element.fire("ajax:failure",  response); }
     });
 
     element.fire("ajax:after");
